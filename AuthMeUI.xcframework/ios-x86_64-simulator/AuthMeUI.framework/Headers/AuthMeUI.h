e05d538a5bf0497b7d39d94f27c0a146fc188d1d
AuthMe Developer <dev@authme.com>
2021-09-29 22:10:15 +0800

    Release 1.2.0

---
+++ AuthMeUI.xcframework/ios-x86_64-simulator/AuthMeUI.framework/Headers/AuthMeUI.h
@@ -0,0 +1,17 @@
+//
+//  AuthMeUI.h
+//  AuthMeUI
+//
+//  Created by Mikimoto on 2021/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthMeUI.
+FOUNDATION_EXPORT double AuthMeUIVersionNumber;
+
+//! Project version string for AuthMeUI.
+FOUNDATION_EXPORT const unsigned char AuthMeUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthMeUI/PublicHeader.h>
+
